60bde3fd68671410a15e6c16b51457d5d3ef1c61
Adam Jack <adamjack@adams-air.attlocal.net>
2018-05-24 18:02:37 -0500

    friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,85 @@
-module.exports = [];
+var friends = [
+    {
+        name: "Burt Macklin",
+        photo: "https://i.ytimg.com/vi/rtbYEfdwEe4/hqdefault.jpg",
+        scores: [
+            "5",
+            "1",
+            "1",
+            "1",
+            "4",
+            "2",
+            "5",
+            "1",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Janet Snakehole",
+        photo: "https://i.pinimg.com/236x/a4/6d/5a/a46d5ae5dddc68995777800a77b116a1--janet-snakehole-cosplay-costumes.jpg",
+        scores: [
+            "4",
+            "4",
+            "3",
+            "1",
+            "5",
+            "5",
+            "5",
+            "1",
+            "2",
+            "1",
+        ]
+    },
+    {
+        name: "Ron Swanson",
+        photo: "http://cdn.playbuzz.com/cdn/173e8dc2-5516-41c1-8e34-43bf6f03b6d8/74395ba4-9272-48b1-8d3f-839a0e53cfef_560_420.jpg",
+        scores: [
+            "1",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "4",
+            "1",
+        ]
+    },
+    {
+        name: "Leslie Knope",
+        photo: "https://img.buzzfeed.com/buzzfeed-static/static/2015-09/16/13/campaign_images/webdr09/leslie-knopes-33-best-lines-on-parks-and-recreati-2-23571-1442423832-1_dblbig.jpg",
+        scores: [
+            "5",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "1",
+            "1",
+            "5",
+        ]
+    },
+    {
+        name: "Tom Haverford",
+        photo: "https://pbs.twimg.com/profile_images/2735326906/3da9756c243939b96564b646d7890ab3_400x400.jpeg",
+        scores: [
+            "5",
+            "3",
+            "5",
+            "4",
+            "2",
+            "5",
+            "5",
+            "1",
+            "1",
+            "3",
+        ]
+    }
+]
+
+
+module.exports = friends;
